fd0fc8e333572a85bda288add67a43f8ae77bc0d
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-16 03:30:44 -0700

    Create sjsj.py

---
+++ sjsj.py
@@ -0,0 +1,10 @@
+string = 'Ayush'
+print(string.isalpha()) 
+   
+  
+string = 'Ayush0212'
+print(string.isalpha()) 
+   
+# checking if space is an alphabet 
+string = 'Ayush Saxena'
+print( string.isalpha()) 
